797f29c5287c80aa5690321f46054eea801cca1e
Elizeus29 <eliseo.cv98@gmail.com>
2021-04-11 18:25:29 -0400

    cambio de variables de conexion

--- config.php
+++ config.php
@@ -1,10 +1,16 @@
 <?php
 
+// $db = [
+//     'host' => 'us-cdbr-east-03.cleardb.com',
+//     'username' => 'bbf819998ccec1',
+//     'password' => 'ff0e12de',
+//     'db' => 'blog' //Cambiar al nombre de tu base de datos
+// ];
 $db = [
-    'host' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'db' => 'blog' //Cambiar al nombre de tu base de datos
+    'host' => 'remotemysql.com',
+    'username' => 'k72Qw6xxSQ',
+    'password' => 'wagI9YRlmA',
+    'db' => 'k72Qw6xxSQ' //Cambiar al nombre de tu base de datos
 ];
 
 ?>
