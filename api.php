fcdd74f94178384c8eaf6a90d6a93029507c58c2
Elizeus29 <82334787+Elizeus29@users.noreply.github.com>
2021-04-12 01:24:14 -0400

    Update api.php

--- api.php
+++ api.php
@@ -5,6 +5,16 @@ include "utils.php";
 
 $dbConn =  connect($db);
 
+// Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+
 /*
   listar todos los vehiculo o solo uno
  */
