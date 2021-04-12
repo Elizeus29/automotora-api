bc966684dfa61d8e710910d4f0f90d7ebb1141e7
Elizeus29 <82334787+Elizeus29@users.noreply.github.com>
2021-04-12 01:26:27 -0400

    Update api.php

--- api.php
+++ api.php
@@ -1,18 +1,16 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+
 include "config.php";
 include "utils.php";
 
 
 $dbConn =  connect($db);
-
-// Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
 
 
 /*
