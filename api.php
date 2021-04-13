6416a9f22e7f7141259e8f0d511a96a170b8458a
Elizeus29 <82334787+Elizeus29@users.noreply.github.com>
2021-04-13 01:31:03 -0400

    Update api.php

--- api.php
+++ api.php
@@ -1,9 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
-header('Content-Type: application/json');
+
 
 include "config.php";
 include "utils.php";
@@ -11,6 +8,7 @@ include "utils.php";
 
 $dbConn =  connect($db);
 
+header('Access-Control-Allow-Origin: *');
 
 /*
   listar todos los vehiculo o solo uno
