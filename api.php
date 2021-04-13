9606271202fdef2927886d67f5f109c9d1c28fbe
Elizeus29 <82334787+Elizeus29@users.noreply.github.com>
2021-04-13 01:32:46 -0400

    Update api.php

--- api.php
+++ api.php
@@ -39,11 +39,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
 // Crear un nuevo post
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {      
-	header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
-	header("HTTP/1.1 200 OK");
-
 	
     $input = $_POST;
     $sql = "INSERT INTO vehiculo
