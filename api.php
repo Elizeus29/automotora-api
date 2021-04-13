acd2dc7b58df47fa82d687df09d01896f3430bab
Elizeus29 <eliseo.cv98@gmail.com>
2021-04-12 20:32:18 -0400

    coreccion api

--- api.php
+++ api.php
@@ -36,16 +36,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     $input = $_POST;
     $sql = "INSERT INTO vehiculo
-          (patente, color, anio, precio, kilometraje, id_marca, id_modelo)
+          (patente, color, anio, precio, kilometros, id_marca, id_modelo)
           VALUES
-          (:patente, :color, :anio, :precio, :kilometraje, :id_marca, :id_modelo)";
+          (:patente, :color, :anio, :precio, :kilometros, :id_marca, :id_modelo)";
     $statement = $dbConn->prepare($sql);
     bindAllValues($statement, $input);
     $statement->execute();
     $postId = $dbConn->lastInsertId();
     if($postId)
     {
-      $input['id'] = $postId;
+      $input['patente'] = $postId;
       header("HTTP/1.1 200 OK");
       echo json_encode($input);
       exit();
